441db88a5e8a92aff4e3f3d571a127291b276bb9
Michael <mzeisberg2000@yahoo.de>
2016-04-30 13:29:11 +0200

    Mailman Support
    Mailman Support

---
+++ distros/debian8/install_mailman.sh
@@ -0,0 +1,41 @@
+InstallMailman (){
+
+echo "================================================================================================"
+echo "You will be prompted for some information during the install."
+echo "Select the languages you want to support and hit OK when told about the missing site list"
+echo "You will also be asked for the email address of person running the list & password for the list."
+echo "Please enter them where needed."
+echo "================================================================================================"
+echo "Press ENTER to continue.."
+read DUMMY
+
+#Install Mailman
+apt-get install -y mailman
+newlist mailman
+
+mv /etc/aliases /etc/aliases.backup
+
+cat > /etc/aliases.mailman <<"EOF"
+mailman:              "|/var/lib/mailman/mail/mailman post mailman"
+mailman-admin:        "|/var/lib/mailman/mail/mailman admin mailman"
+mailman-bounces:      "|/var/lib/mailman/mail/mailman bounces mailman"
+mailman-confirm:      "|/var/lib/mailman/mail/mailman confirm mailman"
+mailman-join:         "|/var/lib/mailman/mail/mailman join mailman"
+mailman-leave:        "|/var/lib/mailman/mail/mailman leave mailman"
+mailman-owner:        "|/var/lib/mailman/mail/mailman owner mailman"
+mailman-request:      "|/var/lib/mailman/mail/mailman request mailman"
+mailman-subscribe:    "|/var/lib/mailman/mail/mailman subscribe mailman"
+mailman-unsubscribe:  "|/var/lib/mailman/mail/mailman unsubscribe mailman"
+EOF
+
+cat /etc/aliases.backup /etc/aliases.mailman > /etc/aliases
+newaliases
+/etc/init.d/postfix restart
+		if [ $web_server == "Apache" ]; then
+				ln -s /etc/mailman/apache.conf /etc/apache2/conf.d/mailman.conf
+				/etc/init.d/apache2 restart
+		fi
+/etc/init.d/mailman start
+
+} # end function debian.install_Mailman
+
